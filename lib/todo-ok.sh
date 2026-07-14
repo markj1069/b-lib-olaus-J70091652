9e8a60747bc104a89de84d2b71bf65c256da219d
Mark J. Jensen <mark@jensen.net>
2026-07-14 09:05:43 -0400

    Add TAP functions.

---
+++ lib/todo-ok.sh
@@ -0,0 +1,14 @@
+#---------------------------------------------------------------------------------------------------
+#
+# Usage:        ok bool test_name
+# Purpose:      ok generates a TAP ok record if bool is true.
+# Returns:      Sets EX_OK exit code
+# Parameteres:  bool: TRUE
+#               test_name: 
+# Throws:       N/A
+# Errors:       N/A
+# Comments:     ok "$a == $b", "Test A equal B."
+# See Also:     N/A
+# Source:       bash
+#
+#---------------------------------------------------------------------------------------------------
